57926cbb90d2d18a3738a11d947153925667bf0f
SatviikDaga <dagasatvik2007@gmail.com>
2026-03-16 23:49:42 +0530

    Create MathExamples.java

---
+++ math_operations/MathExamples.java
@@ -0,0 +1,18 @@
+public class MathExamples {
+
+    public static void main(String[] args) {
+
+        System.out.println("PI: " + Math.PI);
+        System.out.println("E: " + Math.E);
+
+        System.out.println("Power: " + Math.pow(2,3));
+        System.out.println("Square Root: " + Math.sqrt(9));
+        System.out.println("Absolute: " + Math.abs(-5));
+        System.out.println("Round: " + Math.round(3.14));
+        System.out.println("Ceil: " + Math.ceil(4.5));
+        System.out.println("Floor: " + Math.floor(3.6));
+
+        System.out.println("Max: " + Math.max(4,5));
+        System.out.println("Min: " + Math.min(4,5));
+    }
+}
